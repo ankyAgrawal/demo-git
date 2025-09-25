991334725fdb90100d7f13ae3c2ec77addc6dcbf
Ankit Agrawal <agrawala96@gmail.com>
2025-09-25 14:57:03 -0500

    random functions added

--- hello.py
+++ hello.py
@@ -5,7 +5,17 @@ def broadcast_hello_to_entire_class():
     for student in range(1, 31):  # Assuming a class of 30 students
         print(f"Hello, Student {student}!")
 
+def random_function_1():
+    pass
+
+def random_function_2():
+    pass
+def random_function_3():
+    pass
+
+
+
 if __name__ == "__main__":
     say_hello()
     broadcast_hello_to_entire_class()
-    
+
